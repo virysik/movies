1f50b6a2b1acd6e36b9aded357b9f23c7534399b
virysik <68701496+virysik@users.noreply.github.com>
2024-04-11 17:37:02 +0200

    fix: update original img link

--- src/app/ui/movies/list.tsx
+++ src/app/ui/movies/list.tsx
@@ -55,7 +55,7 @@ const MovieImg: FC<{ movie: PopularMovie }> = ({ movie }) => {
         ${config?.images.base_url}${config?.images.backdrop_sizes[1]}${movie.backdrop_path} 780w,
        ${config?.images.base_url}${config?.images.backdrop_sizes[2]}${movie.backdrop_path} 1280w`}
         sizes="(min-width: 768px) calc((100% - 32px*3)/4), (min-width: 420px) calc((100% - 24px)/2), 100vw"
-        src={`${config?.images.base_url}${config?.images.backdrop_sizes[4]}${movie.backdrop_path}`}
+        src={`${config?.images.base_url}${config?.images.backdrop_sizes[3]}${movie.backdrop_path}`}
         alt={movie.title}
         loading="lazy"
       />
